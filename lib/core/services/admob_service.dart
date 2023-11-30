5b495aa9b7c1b2af9e39fdb692692e45c9e37638
ruwiss <bihinmehen@gmail.com>
2023-11-30 20:01:52 +0300

    Reklam yüklemelerinde yükleniyor diyaloğu kaldırıldı

--- lib/core/services/admob_service.dart
+++ lib/core/services/admob_service.dart
@@ -1,5 +1,4 @@
 import 'dart:developer';
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:gunsayaci/core/router.dart';
@@ -9,37 +8,17 @@ class AdmobService {
   final int interstitialShowLimit = 2;
   int interstitialCount = 0;
 
-  static void _showLoadingWidget() {
-    showDialog(
-      context: getContextFromRouter!,
-      builder: (BuildContext context) => AlertDialog(
-        contentPadding: const EdgeInsets.symmetric(horizontal: 8, vertical: 20),
-        content: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            const SizedBox(
-                width: 25, height: 25, child: CircularProgressIndicator()),
-            Text('ad-waiting'.tr(), style: const TextStyle(fontSize: 15)),
-          ],
-        ),
-      ),
-    );
-  }
-
   void callInterstitialAd() {
     if (interstitialShowLimit <= interstitialCount) return;
-    _showLoadingWidget();
     InterstitialAd.load(
       adUnitId: KStrings.insertstitialAdId,
       request: const AdRequest(),
       adLoadCallback: InterstitialAdLoadCallback(
         onAdLoaded: (InterstitialAd ad) {
           interstitialCount++;
-          Navigator.pop(getContextFromRouter!);
           ad.show();
         },
         onAdFailedToLoad: (LoadAdError error) {
-          Navigator.pop(getContextFromRouter!);
           log("Interstitial Ad Load Error : ${error.message}");
         },
       ),
@@ -47,19 +26,16 @@ class AdmobService {
   }
 
   static void callAppOpenAd({Function()? onAction}) {
-    _showLoadingWidget();
     AppOpenAd.load(
       adUnitId: KStrings.appOpenAdId,
       orientation: AppOpenAd.orientationPortrait,
       request: const AdRequest(),
       adLoadCallback: AppOpenAdLoadCallback(
         onAdLoaded: (ad) {
-          Navigator.pop(getContextFromRouter!);
           onAction?.call();
           ad.show();
         },
         onAdFailedToLoad: (error) {
-          Navigator.pop(getContextFromRouter!);
           onAction?.call();
           log("appOpenAd Load Error : ${error.message}");
         },
@@ -67,7 +43,10 @@ class AdmobService {
     );
   }
 
-  static void loadBannerAd({AdSize? adSize, required String adUnitId, Function(BannerAd ad)? onLoaded}) {
+  static void loadBannerAd(
+      {AdSize? adSize,
+      required String adUnitId,
+      Function(BannerAd ad)? onLoaded}) {
     BannerAd(
       adUnitId: adUnitId,
       request: const AdRequest(),
